6f59e7566f83f30f6071846278adbbac25485d24
Cristian Romero <47550226+cir26@users.noreply.github.com>
2019-07-25 17:39:45 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='C. Romero-Fuentes',
     author_email='ciromero26@gmail.com',
     url='https://github.com/cir26/0ptimus-feat_treat',
-    packages=['package'],
+    packages=['Feat_Treat'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )

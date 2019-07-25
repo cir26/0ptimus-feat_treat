08cd32dc04a4615507b0d92eb76ee4550da3ee40
Cristian Romero <47550226+cir26@users.noreply.github.com>
2019-07-25 16:44:36 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='Feat_Treat',
+    name='Feat Treat',
     version='0.1.0',
     author='C. Romero-Fuentes',
     author_email='ciromero26@gmail.com',
     url='https://github.com/cir26/0ptimus-feat_treat',
-    packages=['feat_treat'],
+    packages=['Feat_Treat'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )

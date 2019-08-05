31b279ec869274d81548e9f2ffd3efb7ea3301c5
Cristian Romero <47550226+cir26@users.noreply.github.com>
2019-08-05 10:20:12 -0400

    Add files via upload

--- Feat_Treat/static.py
+++ Feat_Treat/static.py
@@ -203,10 +203,26 @@ class static:
             f2_multi[i]=round(5*((precision_multi[i]*recall_multi[i])/((4*precision_multi[i])+recall_multi[i])),5)
             conf_sum_multi[i] =round(precision_multi[i]+recall_multi[i]+specificity_multi[i]+neg_pred_multi[i],5)
             auc_score_multi[i] = round(roc_auc_score(y_test[:,i], preds[:,i]),5)
+
+        print('weights: ', weights)
+        print('specificity_multi: ', specificity_multi)
+        print('neg_pred_multi: ', neg_pred_multi)
+        print('g1_multi: ', g1_multi)
+        print('accuracy_multi: ',accuracy_multi)
+        print('ck_multi: ',ck_multi)
+        print('mcc_multi: ',mcc_multi)
+        print('f1_multi: ',f1_multi)
+        print('recall_multi: ',recall_multi)
+        print('precision_multi: ',precision_multi)
+        print('f2_multi: ', f2_multi)
+        print('conf_sum_multi: ',conf_sum_multi)
+        print('auc_score_multi: ',auc_score_multi)
+        print('logloss_multi: ',logloss_multi)
+
+
 #       return sum of metrics weighted by class size
-        #accuracy = [x*w for x,w in zip(accuracy_multi,weights)]
-        accuracy = [x for x in accuracy_multi]
-        accuracy= np.mean(accuracy)
+        accuracy = [x*w for x,w in zip(accuracy_multi,weights)]
+        accuracy= sum(accuracy)
         precision = [x*w for x,w in zip(precision_multi,weights)]
         precision = sum(precision)
         recall = [x*w for x,w in zip(recall_multi,weights)]
